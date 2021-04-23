ea4f346609df40804327de0c3c84f75ba98730f4
Tom Nilsson <tomnilsson@Toms-MacBook-Pro.local>
2021-04-23 21:49:28 +0200

    second commit

--- app.js
+++ app.js
@@ -2,24 +2,70 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("superagent");
-const https = require("https");
-
+const mongoose = require("mongoose");
+//const request = require("superagent");
+//const https = require("https");
+const date = require(__dirname + "/date.js");
 const app = express();
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
-  res.set("Content-Type", "text/html");
-  res.sendFile(__dirname + "/index.html");
+console.log(date.getDate());
+
+let userName = "Tom Nilsson"
+
+//connection to tompstomp mongoDB
+mongoose.connect("mongodb+srv://dbUser:dbUserPassword@cluster0.lnn2w.mongodb.net/tompstomp?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.once("open", function(){
+  console.log("connection made");
+}).on("error", function(error){
+  console.log("ERROR IS:", error);
+});
+const stompSchema = new mongoose.Schema({
+  user: String,
+  date: String,
+  text: String
+});
+const Stomp = new mongoose.model("Stomp", stompSchema);
+const stompsArray = [];
+const stompDate = [];
+Stomp.find(function(err, stomps) {
+  if(err) {
+    console.log(err);
+  } else {
+    stomps.forEach(function(stomp){
+      stompsArray.push(stomp.text);
+      stompDate.push(stomp.date);
+    });
+  }
 });
 
+app.get("/", function(req, res) {
+  res.set("Content-Type", "text/html");
+  res.render('index', {date: stompDate, dateToday: date.getDate(), stomps: stompsArray, user: userName });
+});
+
+app.post("/", function(req, res) {
+
+  const stomp = new Stomp({
+    user: "Tom Nilsson",
+    date: date.getDate(),
+    text: req.body.stomp
+  });
+  stomp.save();
+  stompsArray.push(req.body.stomp);
+  stompDate.push(stomp.date);
+  res.redirect("/");
+});
+
+/*
 var mailchimpInstance   = 'us1',
     listUniqueId        = 'c20b4146b8',
-    mailchimpApiKey     = '458303586b26845f8c82f8c9166b8639-us1';
+    mailchimpApiKey     = 'ef51fbdd54bc73bb5eafdbd834a0494b-us1';
 
-app.post('/', function (req, res) {
+app.post('/login', function (req, res) {
     request
         .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
         .set('Content-Type', 'application/json;charset=utf-8')
@@ -40,10 +86,7 @@ app.post('/', function (req, res) {
               }
           });
 
-});
-
-
-
+});*/
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("App running on port 3000");
@@ -53,3 +96,5 @@ app.listen(process.env.PORT || 3000, function(){
 
 //audience id (list-id)
 //c20b4146b8
+
+//mongo "mongodb+srv://cluster0.lnn2w.mongodb.net/myFirstDatabase" --username dbUser
